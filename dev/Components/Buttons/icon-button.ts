b60c0034e310a11608dec6c1d1cf3ceffc6666c5
윤정길 <yunjeong-gil@yunjeong-gil-ui-MacBookPro.local>
2023-11-15 12:55:02 +0900

    테마 색상 변경, 아이콘 버튼 css

--- dev/Components/Buttons/icon-button.ts
+++ dev/Components/Buttons/icon-button.ts
@@ -1,20 +1,46 @@
 import { LitElement, html, css } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { customElement, property } from 'lit/decorators.js';
 
 @customElement('icon-button')
 export class IconButton extends LitElement {
   static override styles = css`
     :host {
-      border: 2px dashed #603ff0;
-      background-color: #3ff054;
       position: relative;
+      font-size: var(--big-font-size);
+
+      color: var(--fifth-color);
+      background-color: var(--first-color);
+
+      border-radius: 25%;
+
+      transition: 0.1s;
     }
-    div {
+    :host(:hover) {
+      background-color: var(--third-color);
+      color: var(--first-color);
+      border: 1px solid var(--first-color);
+
+      transform: scale(1.1);
+      cursor: pointer;
+    }
+    :host(:active) {
+      transform: scale(0.98);
+      cursor: pointer;
+    }
+
+    ion-icon {
       position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
     }
   `;
+
+  @property() icon: string = 'help-outline';
+  @property() key: string = '';
+
   override render() {
-    return html`<div>컨텐츠</div>`;
+    return html` <ion-icon name="${this.icon}"></ion-icon> `;
   }
 }
 
@@ -23,8 +49,3 @@ declare global {
     'icon-button': IconButton;
   }
 }
-
-/*
-
-
-*/
