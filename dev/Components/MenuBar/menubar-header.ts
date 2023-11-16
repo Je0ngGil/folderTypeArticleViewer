7c9d9db76b7b8f86c72be544cc035dbf6af49362
윤정길 <yunjeong-gil@yunjeong-gil-ui-MacBookPro.local>
2023-11-16 13:42:26 +0900

    컴포넌트 이름 변경

--- dev/Components/MenuBar/menubar-header.ts
+++ dev/Components/MenuBar/menubar-header.ts
@@ -1,7 +1,8 @@
 import { LitElement, html, css } from 'lit';
 import { customElement } from 'lit/decorators.js';
 
-import '../Buttons/icon-button';
+import '../Buttons/button-icon';
+import '../Inputs/input-search';
 
 @customElement('menubar-header')
 export class MenuBarHeader extends LitElement {
@@ -21,12 +22,7 @@ export class MenuBarHeader extends LitElement {
     }
     .iconItem {
       width: 25%;
-
-      &::after {
-        content: '';
-        display: block;
-        padding-bottom: 100%;
-      }
+      aspect-ratio: 1;
     }
 
     #inputContainer {
@@ -34,14 +30,21 @@ export class MenuBarHeader extends LitElement {
       height: 50px;
     }
   `;
+
+  test(e: Event) {
+    if (e.currentTarget === e.target) return;
+    console.log((e.target as Element).getAttribute('key'));
+  }
   override render() {
     return html`
-      <div id="iconContainer">
-        <icon-button class="iconItem"></icon-button>
-        <icon-button class="iconItem"></icon-button>
-        <icon-button class="iconItem"></icon-button>
+      <div id="iconContainer" @click=${this.test}>
+        <Button-Icon class="iconItem" icon="caret-back-outline" key="backward"></Button-Icon>
+        <Button-Icon class="iconItem" icon="home-outline" key="home"></Button-Icon>
+        <Button-Icon class="iconItem" icon="caret-forward-outline" key="forward"></Button-Icon>
       </div>
-      <div id="inputContainer">검색바</div>
+      <div id="inputContainer">
+        <Input-Search></Input-Search>
+      </div>
     `;
   }
 }
