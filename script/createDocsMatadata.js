d9be327fe1ebb4564565b82cee3e3f95e84d8ec8
Je0ngGil <wjdrlfgg@gmail.com>
2023-11-29 19:17:36 +0900

    docs metadata 생성 스크립트 작성

---
+++ script/createDocsMatadata.js
@@ -0,0 +1,43 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+function readDirectory(directoryPath) {
+  const stats = fs.statSync(directoryPath);
+
+  if (!stats.isDirectory()) {
+    throw new Error(`${directoryPath} is not a directory.`);
+  }
+
+  const metadata = {
+    name: path.basename(directoryPath),
+    type: 'directory',
+    children: [],
+  };
+
+  const entries = fs.readdirSync(directoryPath);
+
+  entries.forEach((entry) => {
+    const entryPath = path.join(directoryPath, entry);
+    const entryStats = fs.statSync(entryPath);
+
+    if (entryStats.isDirectory()) {
+      const childMetadata = readDirectory(entryPath);
+      metadata.children.push(childMetadata);
+    } else {
+      metadata.children.push({
+        name: entry,
+        type: 'file',
+      });
+    }
+  });
+
+  return metadata;
+}
+
+const currentDirectory = path.join(__dirname + '/../' + '/docs');
+const metadata = readDirectory(currentDirectory);
+
+fs.writeFileSync('./docs-matadata.json', JSON.stringify(metadata, null, 2));
