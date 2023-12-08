372c16de943109eb0c7d388877092aa7f27dbc40
Je0ngGil <wjdrlfgg@gmail.com>
2023-12-08 11:41:44 +0900

    error 확인중

--- dev/main.ts
+++ dev/main.ts
@@ -7,6 +7,7 @@ MenuBarContainerElement.addEventListener('requestContent', (e: any) => {
   let path = e.detail.path;
   //ArticleContainerElement path 전달
   path = '/' + path;
+  console.log(path);
   ArticleContainerElement.renderArticleByPath(path);
 });
 
