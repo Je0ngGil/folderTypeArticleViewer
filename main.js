a29d56fe792e801c7a89c0d4e462e0a7109d8461
Je0ngGil <wjdrlfgg@gmail.com>
2023-12-25 23:59:13 +0900

    [build]

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";const e=document.body,t=document.createElement("menubar-container"),n=document.createElement("article-container");t.addEventListener("requestContent",(e=>{let t=e.detail.path;n.renderArticleByPath(t)})),e.append(t,n)})();
+(()=>{"use strict";const e=document.body,t=document.createElement("menubar-container"),n=document.createElement("article-container");t.addEventListener("requestContent",(e=>{let t=e.detail.path;n.renderArticleByPath(t)})),e.append(t,n),console.log("내용이 바뀌나?")})();
