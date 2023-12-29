33394516868cdbb591033975f6cbbb5de42e52cf
Je0ngGil <wjdrlfgg@gmail.com>
2023-12-29 11:02:33 +0900

    chore: 폴더 구조 변경

---
+++ src/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";const e=document.body,t=document.createElement("menubar-container"),n=document.createElement("article-container");t.addEventListener("requestContent",(e=>{let t=e.detail.path;n.renderArticleByPath(t)})),e.append(t,n)})();
